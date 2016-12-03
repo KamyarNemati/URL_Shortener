ffe421bdb70c513cc6d67ee6d36462a761f841b3
Kamyar Nemati <kamyarnemati@gmail.com>
2016-12-03 15:09:51 +0800

    Home page has been set up.

---
+++ application/views/home/home.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Kamyar
+ */
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>URL_Shortener</title>
+    </head>
+    <body>
+        HelloWorld!
+    </body>
+</html>
